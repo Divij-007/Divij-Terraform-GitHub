755822f52876de2bd44b1808f7a26fe9ea54d9b2
Divij P <divij.10825806@ltimindtree.com>
2025-11-28 10:33:36 +0530

    Updated s3

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,14 +1,33 @@
-resource "aws_s3_bucket" "this" {
+
+resource "aws_s3_bucket" "mys3" {
   bucket = var.bucket_name
-  acl    = var.acl
 
   tags = {
     Name        = var.bucket_name
   }
 }
 
+# Ownership controls (recommended)
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.mys3.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+# Public access block (recommended)
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.mys3.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Versioning
 resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.this.id
+  bucket = aws_s3_bucket.mys3.id
 
   versioning_configuration {
     status = var.versioning_enabled ? "Enabled" : "Suspended"
