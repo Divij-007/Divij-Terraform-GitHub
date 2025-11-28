492e4408b06da2575554454ae73d5c18b1c35c75
Divij P <divij.10825806@ltimindtree.com>
2025-11-28 09:18:19 +0530

    Updated ec2

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,8 +1,8 @@
 
 output "instance_id" {
-  value = aws_instance.this.id
+  value = aws_instance.instance1.id
 }
 
 output "public_ip" {
-  value = aws_instance.this.public_ip
+  value = aws_instance.instance1.public_ip
 }
